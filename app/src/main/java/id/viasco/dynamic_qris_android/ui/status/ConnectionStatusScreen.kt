cdc84f38cd9e8cf9298158f858e16ec039df25d3
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2026-06-26 21:31:35 +0700

    chore: delete unused imports

--- app/src/main/java/id/viasco/dynamic_qris_android/ui/status/ConnectionStatusScreen.kt
+++ app/src/main/java/id/viasco/dynamic_qris_android/ui/status/ConnectionStatusScreen.kt
@@ -10,10 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.CheckCircle
